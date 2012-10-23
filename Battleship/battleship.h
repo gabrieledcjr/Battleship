e351ec3c3018039a89f2c60d6c44ab4d207e6185
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2012-10-22 19:50:28 -0700

    initial commit
    created main.c, battleship.c, battleship.h and coded basic template for
    each file.

---
+++ Battleship/battleship.h
@@ -0,0 +1,10 @@
+#ifndef BATTLESHIP_H
+#define BATTLESHIP_H
+
+#include <stdio.h>
+
+typedef enum {
+	FALSE, TRUE
+} Boolean;
+
+#endif
---
+++ Battleship/main.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+ * Filename: main.c                                                      *
+ * Programmer: Gabriel V. de a Cruz Jr.                                  *
+ * Class: CptS 121, Fall 2012 ; Lab Section 7                            *
+ * Programming Assignment 4: Basic Game of Battleship                    *
+ * Date: October 6, 2012                                                 *
+ *                                                                       *
+ * Description: This program simulates the game of Battleship. The game  *
+ *              will be completely text-based. Battleship is a two       *
+ *              player Navy game. The objective of the game is to sink   *
+ *              all ships in your enemy's fleet. The Player to sink his/ *
+ *              her enemy's fleet first wins. Both players' fleets       *
+ *              consist of 5 ships that are hidden from the enemy. Each  *
+ *              ship may be differentiated by its "size" (besides the    *
+ *              Cruiser and Submarine) or number of cells it expands on  *
+ *              the game board. The Carrier has 5 cells, Battleship has  *
+ *              4 cells, Cruiser has 3 cells, Submarine has 3 cells, and *
+ *              the Destroyer has 2 cells.                               *
+ *************************************************************************/
+#include "battleship.h"
+
+int main (void)
+{
+
+	return 0;
+}
